e2ea08a999f948c5ecc65e3b8eb75868ff868abb
Tomas Garcia <tomasotano25@gmail.com>
2022-10-10 09:13:25 -0400

    Clase 12 Descargando informacion y creando nodos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+const url = 'https://platzi-avo.vercel.app/api/avo';
+
+// web api
+// Conectarnos al server
+// procesar la respuesta y convertirla en JSON
+// JSON -> Data - Renderizar info brwoser
+// promise -> async/await
+async function fetchData() {
+  const response = await window.fetch(url)
+  const responseJson = await response.json();
+
+  console.log(responseJson);
+
+  const allItems = []
+
+  responseJson.data.forEach(item => {
+    console.log(item.name);
+    // crear image
+    const image = document.createElement('img')
+    // crear titulo
+    const title = document.createElement('h2')
+    // crear precio
+    const price = document.createElement('div')
+
+    const container = document.createElement('div')
+    container.append(image, title, price)
+
+    allItems.push(container)
+  })
+
+  document.body.append(...allItems)
+}
+
+fetchData();
